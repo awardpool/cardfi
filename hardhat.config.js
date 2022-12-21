7584bdfb638c307808e6a1a1e867e0512ad3bd40
ruvnet <ruv@ruv.net>
2022-12-21 12:59:10 -0500

    added descriptions

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,21 @@
+// require()
+// The require() function is used to include modules from other files and libraries into a program. It takes a single parameter which is the name of the module to be included.
+
+// @nomiclabs/hardhat-ethers
+// This is an npm package that allows developers to use the Ethers.js library within the Hardhat development environment. It provides a unified interface for interacting with Ethereum networks, wallets and smart contracts.
+
+// @openzeppelin/hardhat-upgrades
+// This is an npm package that allows developers to use the OpenZeppelin library within the Hardhat development environment. It provides a collection of upgradable smart contracts that are used to build secure and reliable decentralized applications on Ethereum.
+
+// @nomiclabs/hardhat-etherscan
+// This is an npm package that allows developers to use the Etherscan API in their Hardhat development environment. It provides an easy way to interact with the Etherscan API, allowing users to quickly and easily access data from the Ethereum blockchain.
+
+// dotenv
+// This is an npm package that allows developers to load environment variables from a .env file into their program. It is used to store sensitive information such as API keys, private keys, and other secure data.
+
+// module.exports
+// This is a special object in a JavaScript program that is used to export functions and variables from one module to another. It is used to make objects, functions, and variables available to other modules in a program.
+
 require("@nomiclabs/hardhat-ethers");
 require("@openzeppelin/hardhat-upgrades");
 require("@nomiclabs/hardhat-etherscan");
