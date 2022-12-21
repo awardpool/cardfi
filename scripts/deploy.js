8b1ab83212fb7f0f0ba1e432970cd30c404fd911
ruvnet <ruv@ruv.net>
2022-12-21 12:51:36 -0500

    added description

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,3 +1,5 @@
+// This code is an example of how to use the ethers and upgrades functions from the hardhat library. The ethers.getContractFactory function creates a contract factory from a given contract name. The upgrades.deployProxy function deploys a proxy contract with a given initializer. The fi.deployed() function deploys the proxy contract and logs the address of the contract to the console.
+
 const { ethers, upgrades } = require("hardhat");
 
 async function main() {
