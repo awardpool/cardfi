912233e7c2d6979fe922bf13167fec930fe91041
Bulava-block <68442071+Bulava-block@users.noreply.github.com>
2022-12-24 00:00:31 -0500

    Update web3switch.js

--- misc/web3switch.js
+++ misc/web3switch.js
@@ -1,13 +1,13 @@
 // Switch blockchains using web3.js
 
-/ Import the web3.js library
-import Web3 from 'web3';
+// Import the web3.js library
+import Web3 from "web3";
 // Create a new instance of the web3.js library
 const web3 = new Web3();
 // Define the function to switch blockchains
 async function switchBlockchain(newNetwork) {
   // Check if the user has MetaMask installed and enabled
-  if (typeof window.ethereum !== 'undefined') {
+  if (typeof window.ethereum !== "undefined") {
     // Request the user's permission to access their MetaMask account
     await window.ethereum.enable();
     // Set the provider for the web3.js library to the user's MetaMask provider
@@ -17,8 +17,8 @@ async function switchBlockchain(newNetwork) {
     // Confirm that the blockchain has been changed
     console.log(`Successfully switched to ${newNetwork} network.`);
   } else {
-    console.log('MetaMask is not installed or enabled.');
+    console.log("MetaMask is not installed or enabled.");
   }
 }
 // Call the function to switch to the Rinkeby testnet
-switchBlockchain('rinkeby');
+switchBlockchain("rinkeby");
